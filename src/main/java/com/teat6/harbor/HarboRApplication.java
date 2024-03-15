a29c5b061e5481685f7946dde8e085232cc25d01
boseok <fa7271@naver.com>
2024-03-15 17:43:48 +0900

    setting java

---
+++ src/main/java/com/teat6/harbor/HarboRApplication.java
@@ -0,0 +1,13 @@
+package com.teat6.harbor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarboRApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarboRApplication.class, args);
+    }
+
+}
