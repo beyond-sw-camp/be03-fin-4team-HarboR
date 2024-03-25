e96d7e4c0ef04744ada4409a1dc91a598e46cd85
SonJM <blisle@naver.com>
2024-03-25 12:32:48 +0900

    [ADD] MSA 폴더 구조 생성

---
+++ HR_member/src/main/java/com/example/hr_login/HrMemberApplication.java
@@ -0,0 +1,13 @@
+package com.example.hr_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrMemberApplication.class, args);
+    }
+
+}
---
+++ HR_member/src/test/java/com/example/hr_login/HrMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hr_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
