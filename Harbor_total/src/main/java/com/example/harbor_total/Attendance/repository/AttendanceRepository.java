001f40b3283d25b4c3a3e054f56833cddb1706f3
boseok <fa7271@naver.com>
2024-04-12 17:51:11 +0900

    [ADD] 휴가 있는지 확인

--- Harbor_total/src/main/java/com/example/harbor_total/Attendance/repository/AttendanceRepository.java
+++ Harbor_total/src/main/java/com/example/harbor_total/Attendance/repository/AttendanceRepository.java
@@ -3,8 +3,9 @@ package com.example.harbor_total.Attendance.repository;
 import com.example.harbor_total.Attendance.domain.Attendance;
 import com.example.harbor_total.Employee.domain.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.data.repository.query.Param;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -16,4 +17,8 @@ public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
     List<Attendance> findByWorkStartTimeBetweenAndEmployeeEmployeeId(LocalDateTime workStartTime, LocalDateTime workStartTime2, String employee_employeeId);
 
     List<Attendance> findAllByEmployee(Employee employee);
+    @Query("SELECT a.attendanceId FROM Attendance a WHERE a.employee.employeeId = :employeeId AND a.workPolicy = :workPolicy")
+    List<Long> findAttendanceIdsByEmployeeIdAndWorkPolicy(@Param("employeeId") String employeeId, @Param("workPolicy") String workPolicy);
+
+
 }
