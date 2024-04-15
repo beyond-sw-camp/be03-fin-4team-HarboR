941ddec02a849ee5e2b67576855b232481041b74
keepself <150704738+keepself@users.noreply.github.com>
2024-04-15 14:23:36 +0900

    [ADD] Schedule make

---
+++ Harbor_employee/src/main/java/com/example/harbor_employee/schedule/repository/ScheduleRepository.java
@@ -0,0 +1,14 @@
+package com.example.harbor_employee.schedule.repository;
+
+import com.example.harbor_employee.schedule.domain.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+        Optional<Schedule> findByScheduleId(Long scheduleId);
+
+}
