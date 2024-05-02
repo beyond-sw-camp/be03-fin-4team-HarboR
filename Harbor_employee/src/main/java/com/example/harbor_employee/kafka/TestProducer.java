47d822a209b7b4bd37d1c5f98ad087a33c3720d3
boseok <fa7271@naver.com>
2024-05-02 17:39:41 +0900

    [ADD] 2024-05-02_Harbor-test20

--- Harbor_employee/src/main/java/com/example/harbor_employee/kafka/TestProducer.java
+++ Harbor_employee/src/main/java/com/example/harbor_employee/kafka/TestProducer.java
@@ -1,31 +1,31 @@
-//package com.example.harbor_employee.kafka;
-//
-//import com.fasterxml.jackson.core.JsonProcessingException;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import lombok.extern.slf4j.Slf4j;
-//import org.apache.poi.ss.formula.functions.T;
-//import org.springframework.kafka.core.KafkaTemplate;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//@Slf4j
-//public class TestProducer {
-//    private final KafkaTemplate<String, Object> kafkaTemplate;
-//    private final ObjectMapper objectMapper;
-//
-//    public TestProducer(KafkaTemplate<String, Object> kafkaTemplate, ObjectMapper objectMapper) {
-//        this.kafkaTemplate = kafkaTemplate;
-//        this.objectMapper = objectMapper;
-//    }
-//
-//    public void sendToKafka(String topic, Object dto) {
-//        String json = "";
-//        try{
-//            json = objectMapper.writeValueAsString(dto);
-//        } catch(JsonProcessingException e){
-//            e.getMessage();
-//        }
-//        kafkaTemplate.send(topic, json);
-//        log.info("Kafka Producer data 전송 완료" + kafkaTemplate);
-//    }
-//}
+package com.example.harbor_employee.kafka;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.formula.functions.T;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class TestProducer {
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    public TestProducer(KafkaTemplate<String, Object> kafkaTemplate, ObjectMapper objectMapper) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public void sendToKafka(String topic, Object dto) {
+        String json = "";
+        try{
+            json = objectMapper.writeValueAsString(dto);
+        } catch(JsonProcessingException e){
+            e.getMessage();
+        }
+        kafkaTemplate.send(topic, json);
+        log.info("Kafka Producer data 전송 완료" + kafkaTemplate);
+    }
+}
--- Harbor_employee/src/main/java/com/example/harbor_employee/kafka/config/KafkaProducerConfig.java
+++ Harbor_employee/src/main/java/com/example/harbor_employee/kafka/config/KafkaProducerConfig.java
@@ -19,8 +19,8 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> config = new HashMap<>();
-//        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        config.put("bootstrap.servers", "b-1.harbormsk.hynstz.c2.kafka.ap-northeast-2.amazonaws.com:9094,b-3.harbormsk.hynstz.c2.kafka.ap-northeast-2.amazonaws.com:9094,b-2.harbormsk.hynstz.c2.kafka.ap-northeast-2.amazonaws.com:9094");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+//        config.put("bootstrap.servers", "b-1.harbormsk.hynstz.c2.kafka.ap-northeast-2.amazonaws.com:9094,b-3.harbormsk.hynstz.c2.kafka.ap-northeast-2.amazonaws.com:9094,b-2.harbormsk.hynstz.c2.kafka.ap-northeast-2.amazonaws.com:9094");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
