03b1458f0b97dc2832c391a540d28968893d55ac
boseok <fa7271@naver.com>
2024-05-02 19:18:42 +0900

    [ADD] 2024-05-02_Harbor-test22

--- Harbor_employee/src/main/java/com/example/harbor_employee/kafka/config/KafkaConsumerConfig.java
+++ Harbor_employee/src/main/java/com/example/harbor_employee/kafka/config/KafkaConsumerConfig.java
@@ -19,8 +19,8 @@ public class KafkaConsumerConfig {
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-//        config.put("bootstrap.servers", "b-3.harbormsk.hynstz.c2.kafka.ap-northeast-2.amazonaws.com:9098,b-1.harbormsk.hynstz.c2.kafka.ap-northeast-2.amazonaws.com:9098,b-2.harbormsk.hynstz.c2.kafka.ap-northeast-2.amazonaws.com:9098");
+//        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put("bootstrap.servers", "b-3.harbormsk.hynstz.c2.kafka.ap-northeast-2.amazonaws.com:9098,b-1.harbormsk.hynstz.c2.kafka.ap-northeast-2.amazonaws.com:9098,b-2.harbormsk.hynstz.c2.kafka.ap-northeast-2.amazonaws.com:9098");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_1");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
