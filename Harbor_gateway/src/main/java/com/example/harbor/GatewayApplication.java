dae1f3293af1b65ec1d8f49c8ab84fccef563838
SonJM <blisle@naver.com>
2024-05-05 22:41:31 +0900

    [DEPLOY] backend-deploy-10

--- Harbor_gateway/src/main/java/com/example/harbor/GatewayApplication.java
+++ Harbor_gateway/src/main/java/com/example/harbor/GatewayApplication.java
@@ -3,13 +3,10 @@ package com.example.harbor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-
 }
