dc538f561ad7066799b32dc9dfb6b925f8a68073
boseok <fa7271@naver.com>
2024-05-05 15:36:57 +0900

    [ADD] 2024-05-04_Harbor-test41

--- Harbor_Login/src/main/java/com/example/harbor_login/GatewayTestController.java
+++ Harbor_Login/src/main/java/com/example/harbor_login/GatewayTestController.java
@@ -7,8 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class GatewayTestController {
-    @GetMapping("/test")
-    public String test() {
-        return "성공";
-    }
+
 }
