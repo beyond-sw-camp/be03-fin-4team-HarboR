62890e84c16ad0e5901bf6d79175fe0588e9b4c0
SonJM <blisle@naver.com>
2024-05-06 13:53:43 +0900

    [DEPLOY] backend-deploy-21

--- Harbor_employee/src/main/java/com/example/harbor_employee/kafka/config/KafkaProducerConfig.java
+++ Harbor_employee/src/main/java/com/example/harbor_employee/kafka/config/KafkaProducerConfig.java
@@ -1,14 +1,18 @@
 package com.example.harbor_employee.kafka.config;
 
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.checkerframework.checker.units.qual.K;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
@@ -33,6 +37,13 @@ public class KafkaProducerConfig {
     @Bean
     public KafkaTemplate<String, Object> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public KafkaAdmin admin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "b-1.harbormsk.jvqi25.c2.kafka.ap-northeast-2.amazonaws.com:9092,b-2.harbormsk.jvqi25.c2.kafka.ap-northeast-2.amazonaws.com:9092");
+        return new KafkaAdmin(configs);
     }
 
     @Bean
