5d7745fc881393869ca8892afdf44ba79fff8efd
keepself <150704738+keepself@users.noreply.github.com>
2024-05-07 10:08:41 +0900

    zz

--- Harbor_employee/src/main/java/com/example/harbor_employee/Stock/StockDomain.java
+++ Harbor_employee/src/main/java/com/example/harbor_employee/Stock/StockDomain.java
@@ -1,22 +1,28 @@
 package com.example.harbor_employee.Stock;
 
 import lombok.Getter;
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "hr_stock")
 @Getter
-public class Stock {
+public class StockDomain {
+
+    @Id
     private Long stockId;
-    private String stockName;            // 이름
-    private String price;                // 현재가
-    private String befordayprice;        // 전일대비
-    private String dayRange;             // 등락률
 
+    @Column
+    private String stockName;
 
+    @Column
+    private String price;
+
+    @Column
+    private String befordayprice;
+
+    @Column
+    private String dayRange;
 }
-
-
-
