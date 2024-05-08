94ae23d98d5aabad321b2c86ca27225568784b57
wontae0924 <wontae0924@gmail.com>
2024-05-09 01:22:43 +0900

    [FIX] 전자 결재 수신 조회 31(콘솔로그)

--- Harbor_frontend/src/views/apps/users/list/AttendanceListRes.vue
+++ Harbor_frontend/src/views/apps/users/list/AttendanceListRes.vue
@@ -19,7 +19,7 @@ async function fetchStatus() {
     setClientHeaders();
     const response = await axios.get(`${baseUrl}/total/annual/read/receive`);
     const tempItems = response.data.result;
-
+    console.log("수신쪽" + tempItems) 
     // 모든 결재자의 이름을 조회합니다.
     const updatedItems = await Promise.all(
       tempItems.map(async (item) => {
