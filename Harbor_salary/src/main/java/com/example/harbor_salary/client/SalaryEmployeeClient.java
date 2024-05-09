ba1094a85b59f0777a7344914e4b551aeb251e1f
wontae0924 <wontae0924@gmail.com>
2024-05-09 10:58:25 +0900

    [FIX] Salary fegin 로컬 삭제 및 재배포

--- Harbor_salary/src/main/java/com/example/harbor_salary/client/SalaryEmployeeClient.java
+++ Harbor_salary/src/main/java/com/example/harbor_salary/client/SalaryEmployeeClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "Employee")
+@FeignClient(name = "employee-service")
 public interface SalaryEmployeeClient {
     @GetMapping("/employee/positionCode")
     GetEmployResponse getPositionCodeByEmployeeId(@RequestHeader("employeeId") String employeeId);
